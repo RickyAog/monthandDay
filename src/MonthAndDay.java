4692769992c3d6fe9693db6ce4f22f32cecec596
Ricky <richard.anderson.2021@student.lhschools.org>
2019-04-05 14:13:46 -0400

    Started first Switch statement

--- src/MonthAndDay.java
+++ src/MonthAndDay.java
@@ -5,7 +5,7 @@ public static void main (String[] args){
     Scanner keyboard = new Scanner(System.in);
     System.out.println(birthDate(keyboard));
 }
-public static Scanner birthDate(Scanner dateBorn){
+public static String birthDate(Scanner dateBorn){
 System.out.println("There is an old saying");
 
 System.out.println("Monday's child is fair of face,");
@@ -18,11 +18,48 @@ System.out.println("Saturday's child works hard for a living,\n" +
         "Is fair and wise and good in every way.");
 
 System.out.println("Now may I ask what month were you born");
-    String monthBorn = dateBorn.nextLine();
+    int monthBorn = dateBorn.nextInt();
+switch(monthBorn){
+    case 1:
+        return "January";
+        break;
+    case 2:
+        return "February";
+        break;
+    case 3:
+        return"March";
+        break;
+    case 4:
+        return "April";
+        break;
+    case 5:
+        return "May";
+        break;
+    case 6:
+        return "June";
+        break;
+    case 7:
+        return "July";
+        break;
+    case 8:
+       return "August";
+        break;
+    case 9:
+        return "September";
+        break;
+    case 10:
+        return "October";
+        break;
+    case 11:
+        return "November";
+        break;
+    case 12:
+        return "December";
+        break;
+}
 
 
-return(dateBorn);
-
+    return monthBorn;
 
     }
 }
