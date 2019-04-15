78776bd98cc34433cb3223939cad5ed95fd285f8
Ricky <richard.anderson.2021@student.lhschools.org>
2019-04-15 14:03:40 -0400

    Changed the code a bit

--- src/MonthAndDay.java
+++ src/MonthAndDay.java
@@ -1,65 +1,83 @@
 import java.util.Scanner;
 
 public class MonthAndDay {
-public static void main (String[] args){
-    Scanner keyboard = new Scanner(System.in);
-    System.out.println(birthDate(keyboard));
-}
-public static String birthDate(Scanner dateBorn){
-System.out.println("There is an old saying");
-
-System.out.println("Monday's child is fair of face,");
-System.out.println("Tuesday's child is full of grace,,");
-System.out.println("Wednesday's child is full of woe,");
-System.out.println("Thursday's child has far to go.");
-System.out.println("Friday's child is loving and giving,");
-System.out.println("Saturday's child works hard for a living,\n" +
-        "But the child born on the Sabbath Day,\n" +
-        "Is fair and wise and good in every way.");
-
-System.out.println("Now may I ask what month were you born");
-    int monthBorn = dateBorn.nextInt();
-switch(monthBorn){
-    case 1:
-        return "January";
-        break;
-    case 2:
-        return "February";
-        break;
-    case 3:
-        return"March";
-        break;
-    case 4:
-        return "April";
-        break;
-    case 5:
-        return "May";
-        break;
-    case 6:
-        return "June";
-        break;
-    case 7:
-        return "July";
-        break;
-    case 8:
-       return "August";
-        break;
-    case 9:
-        return "September";
-        break;
-    case 10:
-        return "October";
-        break;
-    case 11:
-        return "November";
-        break;
-    case 12:
-        return "December";
-        break;
-}
+    public static void main(String[] args) {
+    Scanner keyboard= new Scanner(System.in);
+    boolean loop = true;
+    while(loop){
+   if(birthMonth(keyboard))
+       System.out.println("What day were you born (Sunday-Saturday");
+   System.out.println(birthDay(keyboard));
+   loop = !loop;
+    }
 
 
-    return monthBorn;
 
+
+    }
+    public static boolean birthMonth(Scanner keyboard){
+        System.out.println("There is an old  nursery rhyme");
+        System.out.println("Monday's child is fair of face,\n" +
+                "Tuesday's child is full of grace,\n" +
+                "Wednesday's child is full of woe,\n" +
+                "Thursday's child has far to go.\n" +
+                "Friday's child is loving and giving,\n" +
+                "Saturday's child works hard for a living,\n" +
+                "But the child born on the Sabbath Day,\n" +
+                "Is fair and wise and good in every way.");
+        System.out.println("What month were you born (enter as a number please)");
+        int month = keyboard.nextInt();
+        switch(month){
+            case 1:
+                System.out.println("So you were born in January");
+                break;
+            case 2:
+                System.out.println("So you were born in February");
+                break;
+            case 3:
+                System.out.println("So you were born in March");
+                break;
+            case 4:
+                System.out.println("So you were born in April");
+                break;
+            case 5:
+                System.out.println("So you were born in May");
+                break;
+            case 6:
+                System.out.println("So you were born in June");
+                break;
+            case 7:
+                System.out.println("So you were born in July");
+                break;
+            case 8:
+                System.out.println("So you were born in August");
+                break;
+            case 9:
+                System.out.println("So you were born in September");
+                break;
+            case 10:
+                System.out.println("So you were born in October");
+                break;
+            case 11:
+                System.out.println("So you were born in November");
+                break;
+            case 12:
+                System.out.println("So you were born in December");
+                break;
+                default:
+System.out.println("Wow I had no idea there was that many months in 1 year");
+
+        }
+        return false;
+
+    }
+    public static boolean birthDay(Scanner keyboard){
+        System.out.println("I would like to know what day you were born");
+    int day = keyboard.nextInt();
+        switch(day){
+
+    }
+
+ return false;
     }
 }
