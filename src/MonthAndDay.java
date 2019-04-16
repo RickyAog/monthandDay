b1acae0d1385de8474a189d2ceaee210c1401e0d
Ricky <richard.anderson.2021@student.lhschools.org>
2019-04-16 14:17:24 -0400

    Added the year method just need to calculate

--- src/MonthAndDay.java
+++ src/MonthAndDay.java
@@ -8,6 +8,9 @@ public class MonthAndDay {
    if(birthMonth(keyboard))
        System.out.println("What day were you born (Sunday-Saturday");
    System.out.println(birthDay(keyboard));
+   System.out.println("What is the year you were born");
+   System.out.println(yearBorn(keyboard));
+
    loop = !loop;
     }
 
@@ -75,9 +78,98 @@ System.out.println("Wow I had no idea there was that many months in 1 year");
         System.out.println("I would like to know what day you were born");
     int day = keyboard.nextInt();
         switch(day){
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                break;
+            case 9:
+                break;
+            case 10:
+                break;
+            case 11:
+                break;
+            case 12:
+                break;
+            case 13:
+                break;
+            case 14:
+                break;
+            case 15:
+                break;
+            case 16:
+                break;
+            case 17:
+                break;
+            case 18:
+                break;
+            case 19:
+                break;
+            case 20:
+                break;
+            case 21:
+                break;
+            case 22:
+                break;
+            case 23:
+                break;
+            case 24:
+                break;
+            case 25:
+                break;
+            case 26:
+                break;
+            case 27:
+                break;
+            case 28:
+                break;
+            case 29:
+                break;
+            case 30:
+                break;
+            case 31:
+                break;
+
+
+
+default:
+    System.out.println("That is not a real day");
+        }
+
+        return false;
+    }
+    public static boolean yearBorn(Scanner keyboard){
+    int year = keyboard.nextInt();
+    switch(year){
+        case 2000:
+            System.out.println("2000");
+        case 2001:
+            System.out.println("2001");
+        case 2002:
+            System.out.println("2002");
+        case 2003:
+            System.out.println("2003");
+        case 2004:
+            System.out.println("2004");
+        default:
+            System.out.println("You were born before 2000");
+
+
+
 
     }
-
- return false;
+    return false;
     }
+
 }
